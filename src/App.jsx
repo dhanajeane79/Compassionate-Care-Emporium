afac6d08f375f8f51d38f959e251b916009b7cea
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-29 08:38:47 -0400

    Added Home Route Path

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
+import React, { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
 import "./index.css";
 import NavBar from "./Components/NavBar";
 import Home from "./Components/Home";
-import AllProducts from "./Components/AllProducts";
-import RegistrationForm from "./Components/RegistrationForm";
-import Login from "./Components/Login";
+// import AllProducts from "./Components/AllProducts";
+// import RegistrationForm from "./Components/RegistrationForm";
+// import Login from "./Components/Login";
 
-export default function App() {
- 
+
+
+
+function App() {
   return (
     <Router>
-      <div className="app">
+    <div>
       <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<AllProducts />} />
-          <Route path="/register" element={<RegistrationForm />} />
-          <Route path="/login" element={<Login  />} />
-        </Routes>
-      </div>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
     </Router>
   );
 }
+
+export default App;
