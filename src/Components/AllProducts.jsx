914c56cf37025086c780cb84be4a4a78c58b7889
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-31 18:57:41 -0400

    Added fetchProducts function for simple list of products

--- src/Components/AllProducts.jsx
+++ src/Components/AllProducts.jsx
@@ -9,44 +9,57 @@
 import React, { useState, useEffect } from "react";
 
 
-function AllProducts() {
+function AllProducts({ BASE_URL, token }) {
 
   const [products, setProducts] = useState([]);
-  const [user, setUser] = useState({});
-  const [searchTerm, setSearchTerm] = useState("");
+//   const [user, setUser] = useState({});
+//   const [searchTerm, setSearchTerm] = useState("");
 
   // Fetch products when the component mounts
   useEffect(() => {
-    fetchUser();
+    async function fetchProducts() {
+      try {
+        const response = await fetch(`${BASE_URL}/products`, {
+          headers: {
+            Authorization: `Bearer ${token}` // Include the token in the request header
+          }
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setProducts(data);
+        } else {
+          console.error('Failed to fetch products');
+        }
+      } catch (error) {
+        console.error('Error while fetching products', error);
+      }
+    }
+
     fetchProducts();
-  }, [token]);
-
-  const fetchProducts = async () => {
-    fetch(`${BASE_URL}/products`)
-      .then((response) => response.json())
-      .then((data) => {
-        setProducts(data.data.products);
-      })
-      .catch((error) => {
-        console.error("Error fetching products:", error);
-      });
-  };
-
+  }, [BASE_URL, token]);
  // Function for matching text in searh bar
- const productMatches = (product, text) => {
-    return (
-      product.title.toLowerCase().includes(text) ||
-      product.description.toLowerCase().includes(text)
-    );
-  };
-  const filteredProducts = products.filter((product) =>
-    productMatches(product, searchTerm.toLowerCase())
-  );
-  const productsToDisplay = searchTerm.length ? filteredProducts : products;
+//  const productMatches = (product, text) => {
+//     return (
+//       product.title.toLowerCase().includes(text) ||
+//       product.description.toLowerCase().includes(text)
+//     );
+//   };
+//   const filteredProducts = products.filter((product) =>
+//     productMatches(product, searchTerm.toLowerCase())
+//   );
+//   const productsToDisplay = searchTerm.length ? filteredProducts : products;
 
-  return 
-    
-  
+return (
+    <div>
+      <h1>All Products</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default AllProducts;
