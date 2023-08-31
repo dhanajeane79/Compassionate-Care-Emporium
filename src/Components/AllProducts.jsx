44adce91d01b30d2bc5a79cfab595064c4c26e9e
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-31 18:35:33 -0400

    Transferred ST AllProducts Code

--- src/Components/AllProducts.jsx
+++ src/Components/AllProducts.jsx
@@ -7,23 +7,46 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
-import ProductCard from './ProductCard';
 
 
 function AllProducts() {
 
   const [products, setProducts] = useState([]);
+  const [user, setUser] = useState({});
+  const [searchTerm, setSearchTerm] = useState("");
 
-  return (
-    <div className="all-products">
-      <h2>All Products</h2>
-      <div className="product-list">
-        {products.map(product => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
-    </div>
+  // Fetch products when the component mounts
+  useEffect(() => {
+    fetchUser();
+    fetchProducts();
+  }, [token]);
+
+  const fetchProducts = async () => {
+    fetch(`${BASE_URL}/products`)
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data.data.products);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  };
+
+ // Function for matching text in searh bar
+ const productMatches = (product, text) => {
+    return (
+      product.title.toLowerCase().includes(text) ||
+      product.description.toLowerCase().includes(text)
+    );
+  };
+  const filteredProducts = products.filter((product) =>
+    productMatches(product, searchTerm.toLowerCase())
   );
+  const productsToDisplay = searchTerm.length ? filteredProducts : products;
+
+  return 
+    
+  
 }
 
 export default AllProducts;
