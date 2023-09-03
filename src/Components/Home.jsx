4e296f317ba1c8c89de92a480d60cf43a0596bde
dhanajeane79 <dhanajeane79@gmail.com>
2023-09-03 17:18:33 -0400

    Added placeholder text to the Welcome Statement and a Shop Now Button

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable react/no-unescaped-entities */
+
+import { Link } from "react-router-dom";
 function Home() {
     return (
       <div className="home-container">
         <header className="home-header">
           <h1>Welcome</h1>
-          <p>Short Description</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </header>
+
+        <Link to="/products" className="shop-now-button">
+          <button className="btn btn-primary">Shop Now</button>
+        </Link>
+
         <section className="home-features">
           <div className="feature">
             <h2>Care for Them</h2>
