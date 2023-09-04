9a88aab7b0da857822d2201b06db4f1559d40085
dhanajeane79 <dhanajeane79@gmail.com>
2023-09-04 10:31:55 -0400

    Added Product Category Cards

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,43 +1,46 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unescaped-entities */
-
+import React from 'react';
 import { Link } from "react-router-dom";
+import "../CSS-Components/Home.css"
 function Home() {
-    return (
-      <div className="home-container">
-        <header className="home-header">
-          <h1>Welcome</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </header>
+  const categoryCards = Array.from({ length: 6 }, (_, index) => index + 1); // Create an array of card numbers from 1 to 6
+  return (
+    <div className="home-container">
+      <header className="home-header">
+        <h1>Welcome to Compassionate Care Emporium</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </header>
 
-        <Link to="/products" className="shop-now-button">
-          <button className="btn btn-primary">Shop Now</button>
-        </Link>
-
-        <section className="home-features">
-          <div className="feature">
-            <h2>Care for Them</h2>
-            <p>Discover a wide range of items for your loved one</p>
-           
+      <Link to="/products" className="shop-now-button">
+        <button className="btn btn-primary">Shop Now</button>
+      </Link>
+<div className= "category-card-container">
+      <section className="top-category-row">
+        {categoryCards.slice(0, 3).map((cardNumber) => (
+          <div key={cardNumber} className="category-card">
+            <h3>Category {cardNumber}</h3>
+            {/* Add other content for the category card */}
           </div>
-          <div className="feature">
-            <h2>Care for You</h2>
-            <p>Discover a wide range of items for YOU</p>
-            
+        ))}
+      </section>
+
+      <section className="bottom-category-row">
+        {categoryCards.slice(3).map((cardNumber) => (
+          <div key={cardNumber} className="category-card">
+            <h3>Category {cardNumber}</h3>
+            {/* Add other content for the category card */}
           </div>
-          <div className="feature">
-            <h2>Connect</h2>
-            <p>Connect with Others on the same Journey</p>
-            
-          </div>
-        </section>
-
-
-
-        <footer className="home-footer">
-          <p>&copy;All rights reserved.</p>
-        </footer>
+        ))}
+      </section>
       </div>
-    );
-  }
-  
-  export default Home;
+      <footer className="home-footer">
+        <p>&copy; All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
