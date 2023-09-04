e4f76b9ddd52656cda123b1f2395a8d689d3c754
dhanajeane79 <dhanajeane79@gmail.com>
2023-09-04 10:51:35 -0400

    Testing different Nav Bar

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import NavBar from './NavBar'; // Replace with the path to your Navbar component
+import TestNav from './NavBar'; // Replace with the path to your Navbar component
 import '../CSS-Components/StyleLayout.css'; // Import the CSS file for the layout styles
 
 function Layout({ children }) {
   return (
     <div className="layout-container">
-      <NavBar />
+      <TestNav />
    
       <main className="page-content">
         {children}
